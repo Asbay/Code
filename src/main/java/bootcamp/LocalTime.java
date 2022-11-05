911fcc42cbe50334246209d318193d92a2c0fdcb
AsBay <asenabayhan@outlook.de>
2022-11-05 23:08:38 +0100

    degisiklik

--- src/main/java/bootcamp/LocalTime.java
+++ src/main/java/bootcamp/LocalTime.java
@@ -4,10 +4,10 @@ public class LocalTime {
 
     public static void main(String[] args) {
 
-        //2520, 1'den 10'a kadar olan sayıların her birine kalansız
-//        bölünebilen en küçük sayıdır.
-//        1'den 20'ye kadar olan sayıların tamamına tam bölünebilen en küçük pozitif sayı
+/*  1'den 20'ye kadar olan sayıların tamamına tam bölünebilen en küçük pozitif sayı
 //        kaçtır
+Was ist die kleinste positive Zahl, die durch alle Zahlen von 1 bis 12 teilbar ist?
+        */
                 int top = 0;
                 int j=21;
 
