47879ad8a13b3a21017a4e896b63f53557051690
AsBay <asenabayhan@outlook.de>
2022-11-08 19:59:26 +0100

    neu

---
+++ src/main/java/KasimYedi/YınelemelıDızı.java
@@ -0,0 +1,46 @@
+package KasimYedi;
+
+public class YınelemelıDızı {
+    /*
+    Pozitif tamsayılar kümesi için aşağıdaki yinelemeli dizi tanımlanır:
+
+                     n → n/2 (n çifttir)
+
+                     n → 3n + 1 (n tektir)
+
+
+Yukarıdaki kuralı kullanarak ve 13’den başlayarak aşağıdaki diziyi oluşturuyoruz:
+
+                   13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+
+""Bu dizinin (13'ten başlayıp 1'de biten) 10 terim içerdiği görülebilir.
+
+Henüz kanıtlanmamış olmasına rağmen (Collatz Problemi), tüm başlangıç sayılarının
+
+ 1'de bittiği düşünülmektedir.""
+
+
+Bir milyonun altındaki hangi başlangıç sayısı en uzun zinciri oluşturur?
+
+NOT: Zincir başladıktan sonra terimler bir milyonun üzerine çıkabilir.
+     */
+    public static void main(String[] args) {
+        int y=13;
+        do {y=cift(y);
+            System.out.print(y+" ");
+
+        }while (y!=1);
+
+
+
+
+    }public static int cift(int a){
+        if (a%2==0){
+            return a/2;}else return a*3+1;
+
+    }
+
+
+}
+
