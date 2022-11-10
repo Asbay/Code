c8abfdc1a1d1f46c77865349ff10fd4fcdd64019
AsBay <asenabayhan@outlook.de>
2022-11-10 19:27:48 +0100

    neu

---
+++ src/main/java/KasimYedi/SayilariKelimeOlarakYazdirma.java
@@ -0,0 +1,16 @@
+package KasimYedi;
+
+public class SayilariKelimeOlarakYazdirma {
+
+    public static void main(String[] args) {
+
+        int sayi=0;
+        switch (sayi){
+            case 1 :
+                System.out.println("Bir");
+            case 2:
+                System.out.println("Iki");
+
+        }
+    }
+}
