da7964dffcde247aa6a78c7c90d14a9559229ec9
AsBay <asenabayhan@outlook.de>
2023-03-07 22:06:32 +0100

    07.03

---
+++ src/main/java/HackerRank/Einmaleins.java
@@ -0,0 +1,22 @@
+package HackerRank;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Einmaleins {
+
+        public static void main(String[] args)  {
+
+int N =2;
+            int i =1;
+            while(i<=10){
+                System.out.println(N+" "+"x"+" "+i+" "+"="+" "+ i*N);
+                i++;
+            }
+
+
+
+        }
+    }
+
+
---
+++ src/main/java/HackerRank/Solution.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner scan= new Scanner(System.in);
+            int k = scan.nextInt();
+            int l = scan.nextInt();
+            int m = scan.nextInt();
+
+            System.out.println(k);
+            System.out.println(l);
+            System.out.println(m);
+
+
+
+        }
+    }
+
