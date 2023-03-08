a282e55aaad039f1b0f232198331aac18ca39d32
AsBay <asenabayhan@outlook.de>
2023-03-08 21:17:28 +0100

    08.03

---
+++ src/main/java/HackerRank/JavaEndOfFile.java
@@ -0,0 +1,15 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line_num = 0;
+        while(sc.hasNext())
+            System.out.println(++line_num + " " + sc.nextLine());
+        sc.close();
+
+    }
+}
---
+++ src/main/java/HackerRank/JavaIntToString.java
@@ -0,0 +1,12 @@
+package HackerRank;
+
+public class JavaIntToString {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        int number = -782;
+        String numberAsString = String.valueOf(number);
+        System.out.println(numberAsString);
+    }
+}
